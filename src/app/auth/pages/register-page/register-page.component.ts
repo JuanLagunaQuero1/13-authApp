4258d454110a7cd8eaa550d8cd0ad170f1b0b02a
JuanLagunaQuero1 <juanlagunaquero@gmail.com>
2024-12-17 13:35:53 +0100

    Creación páginas Login y Register

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.css'
 })
 export class RegisterPageComponent {
+
+  private fb = inject(FormBuilder);
+
+  public myForm: FormGroup = this.fb.group({
+    name:     ['', [ Validators.required]],
+    email:    ['', [ Validators.required, Validators.email]],
+    password: ['', [ Validators.required, Validators.minLength(6)]],
+  })
+
+  register() {
+    console.log(this.myForm.value);
+
+  }
 
 }
